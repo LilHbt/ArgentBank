f1714a45d7023df12aba4caf49e2eeca88f222db
LilHbt <lilianhumblot@gmail.com>
2024-03-02 12:14:23 +0100

    css finished

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
 
 function ErrorPage() {
-  return <div>ErrorPage 404</div>;
+  return (
+    <div className="error">
+      <h1 className="error--title">404</h1>
+      <p className="error--text">This page doesn't exist!</p>
+      <Link className="error--link" to={"/"}>
+        Get back to Home page
+      </Link>
+    </div>
+  );
 }
 
 export default ErrorPage;
