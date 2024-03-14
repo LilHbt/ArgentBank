546a41da4b001b6e53128190103f495a210ce9a6
LilHbt <lilianhumblot@gmail.com>
2024-03-14 13:27:11 +0100

    last change

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import BannerText from "./BannerText";
-import imgBanner from "../../assets/img/bank-tree.jpeg";
+import imgBanner from "../../assets/img/bank-tree.webp";
 import "./Banner.scss";
 
 function Banner() {
   return (
     <div className="banner">
-      <img className="banner--img" src={imgBanner} alt="Little money plant" />
+      <img
+        className="banner--img"
+        src={imgBanner}
+        alt="Little money plant"
+        rel="preload"
+      />
       <BannerText />
     </div>
   );
